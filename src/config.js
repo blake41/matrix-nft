1190a5a844f9d9f45b584c013694e8f44206bc97
blake johnson <blake41@gmail.com>
2021-03-09 09:38:36 -0600

    fixed contract build and deploy and fixed config file

--- src/config.js
+++ src/config.js
@@ -1,28 +1,12 @@
 const CONTRACT_NAME = 'my-first-nft.blakeneartest.testnet'
 module.exports = function getConfig() {
 	let config = {
-		networkId: 'default',
+		networkId: 'testnet',
 		nodeUrl: 'https://rpc.testnet.near.org',
 		walletUrl: 'https://wallet.testnet.near.org',
 		helperUrl: 'https://helper.testnet.near.org',
-		CONTRACT_NAME,
+		contractName: CONTRACT_NAME
 	};
-
-	if (process.env.REACT_APP_ENV !== 'prod') {
-		config = {
-			...config,
-			GAS: '300000000000000',
-			DEFAULT_NEW_ACCOUNT_AMOUNT: '5',
-			contractMethods: {
-				changeMethods: ['new', 'mint_token', 'guest_mint', 'transfer', 'set_price', 'purchase', 'withdraw'],
-				viewMethods: ['get_token_data', 'get_num_tokens', 'get_proceeds'],
-			},
-			accessKeyMethods: {
-				changeMethods: ['guest_mint', 'set_price', 'withdraw'],
-				viewMethods: ['get_token_data', 'get_num_tokens', 'get_proceeds', 'get_pubkey_minted'],
-			},
-		};
-	}
 
 	return config;
 };

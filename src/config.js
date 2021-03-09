b3a2b1f278be8417404ffe088bb2420870289886
blake johnson <blake41@gmail.com>
2021-03-08 22:05:18 -0600

    change where wallet points to

--- src/config.js
+++ src/config.js
@@ -4,13 +4,13 @@ module.exports = function getConfig() {
 	let config = {
 		networkId: 'default',
 		nodeUrl: 'https://rpc.testnet.near.org',
-		walletUrl: 'http://localhost:1234',
-		// walletUrl: 'https://wallet.testnet.near.org',
+		// walletUrl: 'http://localhost:1234',
+		walletUrl: 'https://wallet.testnet.near.org',
 		helperUrl: 'https://helper.testnet.near.org',
 		contractName,
 	};
-    
-	if (process.env.REACT_APP_ENV !== undefined) {
+
+	if (process.env.REACT_APP_ENV !== 'prod') {
 		config = {
 			...config,
 			GAS: '300000000000000',
@@ -25,17 +25,17 @@ module.exports = function getConfig() {
 			},
 		};
 	}
-    
-	if (process.env.REACT_APP_ENV === 'prod') {
-		config = {
-			...config,
-			networkId: 'mainnet',
-			nodeUrl: 'https://rpc.mainnet.near.org',
-			walletUrl: 'https://wallet.near.org',
-			helperUrl: 'https://helper.mainnet.near.org',
-			contractName: 'near',
-		};
-	}
+
+	// if (process.env.REACT_APP_ENV === 'prod') {
+	// 	config = {
+	// 		...config,
+	// 		networkId: 'mainnet',
+	// 		nodeUrl: 'https://rpc.mainnet.near.org',
+	// 		walletUrl: 'https://wallet.near.org',
+	// 		helperUrl: 'https://helper.mainnet.near.org',
+	// 		contractName: 'near',
+	// 	};
+	// }
 
 	return config;
 };

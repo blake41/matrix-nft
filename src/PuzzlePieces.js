af6a80f1ce2e15a27408ce0eb3bfeedfe43c0e42
blake johnson <blake41@gmail.com>
2021-03-09 16:19:41 -0600

    pattern for secret tiles just repeats now

--- src/PuzzlePieces.js
+++ src/PuzzlePieces.js
@@ -7,15 +7,16 @@ import LostThree from './img/lost3.jpeg';
 
 function Piece(props) {
   const secretUrl = {
-    1: LostOne,
-    2: LostTwo,
-    3: LostThree
+    0: LostOne,
+    1: LostTwo,
+    2: LostThree
   }
 
+  const key = props.tokenId % 3
   return (
     <div className="puzzle-piece">
       <img className="nft-main" src={props.src}></img>
-      <img className="nft-secret" src={secretUrl[props.secretUrl]}></img>
+      <img className="nft-secret" src={secretUrl[key]}></img>
     </div>
   )
 }
@@ -25,7 +26,7 @@ export default function PuzzlePieces(props) {
   const Pieces = piecesData.sort(function(a, b) {
     return a.token_id - b.token_id;
   }).map((piece, i) => {
-    return <Piece key={i} src={piece.metadata} secretUrl={piece.token_id}/>
+    return <Piece key={i} src={piece.metadata} tokenId={piece.token_id}/>
   })
   return(
     <div>{Pieces}</div>

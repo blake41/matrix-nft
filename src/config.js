cb809b1f9ceeb7a4dfc470a1a30f4ee6edc9704d
blake johnson <blake41@gmail.com>
2021-03-08 22:22:15 -0600

    update config

--- src/config.js
+++ src/config.js
@@ -4,7 +4,6 @@ module.exports = function getConfig() {
 	let config = {
 		networkId: 'default',
 		nodeUrl: 'https://rpc.testnet.near.org',
-		// walletUrl: 'http://localhost:1234',
 		walletUrl: 'https://wallet.testnet.near.org',
 		helperUrl: 'https://helper.testnet.near.org',
 		contractName,
@@ -25,17 +24,6 @@ module.exports = function getConfig() {
 			},
 		};
 	}
-
-	// if (process.env.REACT_APP_ENV === 'prod') {
-	// 	config = {
-	// 		...config,
-	// 		networkId: 'mainnet',
-	// 		nodeUrl: 'https://rpc.mainnet.near.org',
-	// 		walletUrl: 'https://wallet.near.org',
-	// 		helperUrl: 'https://helper.mainnet.near.org',
-	// 		contractName: 'near',
-	// 	};
-	// }
 
 	return config;
 };
